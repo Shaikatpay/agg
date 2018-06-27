ea947b3a1ac33d672da91ee50b68976bb8f04b68
Shariful Islam <shaikatpay@gmail.com>
2018-06-27 23:37:32 +0600

    Update script.js

--- assets/dev/js/script.js
+++ assets/dev/js/script.js
@@ -1,1 +1,11 @@
 
+$(document).ready(function() {
+
+  // Returns height of browser viewport
+  $(window).on('resize.windowscreen', function() {
+    $('.windowscreen').height($(this).height());
+  });
+
+  $(window).trigger('resize.windowscreen');
+
+});
